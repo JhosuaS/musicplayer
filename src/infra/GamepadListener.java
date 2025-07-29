d19851536dd070d6876305ac7d27a3e4751e52be
JosuePQ2005 <walter.pizarro@epn.edu.ec>
2025-07-28 21:29:15 -0600

    Add files via upload

---
+++ src/infra/GamepadListener.java
@@ -0,0 +1,15 @@
+package infra;
+
+public interface GamepadListener {
+    
+    void onStop();     // a
+    void onNext(); //dpadRight
+    void onPrevious(); //dpadleft
+    void onVolUp(); //RB
+    void onVolDown(); //LB
+    void onPreviousPag();  //y
+    void onSelect(); // x
+void onCrossUp();
+void onCrossDown();
+
+}
